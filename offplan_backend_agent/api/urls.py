37d98a20fba7b3276f1f0f7c378e75a7d5e6e57d
sreehari-a-h <sreehari.delemon@gmail.com>
2025-07-16 12:42:35 +0530

    refactor: updated backend-urls v1.1.

--- offplan_backend_agent/api/urls.py
+++ offplan_backend_agent/api/urls.py
@@ -1,12 +1,37 @@
-# from rest_framework.routers import DefaultRouter
-# # from api.views.admin_agent_viewset import AdminAgentDetailViewSet
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from api.views.agent_details_username import AgentDetailByUsernameView
+from api.views.property_filter import FilterPropertiesView
+from api.views.properties_list import PropertyListView
+from api.views.property_details import PropertyDetailView
+from api.views.cities_list import CityListView
+from api.views.agent_register import AgentRegisterView
+from api.views.agent_update import AgentUpdateView
+from api.views.agent_delete import AgentDeleteView
+from api.views.property_status_counts import PropertyStatusCountView
+from api.views.property_city_count import PropertyByStatusView
+from api.views.consultation import ConsultationView
+from api.views.subscription import SubscribeView
+from api.views.developers_list import DeveloperListView
+from api.views import AgentListView
 
-# router = DefaultRouter()
-# # router.register(r'admin/agents', AdminAgentDetailViewSet, basename='admin-agents')
+router = DefaultRouter()
+# router.register(r'admin/agents', AdminAgentDetailViewSet, basename='admin-agents')
 
-# urlpatterns = router.urls
-
-# urlpatterns = [
-#     ...
-#     path('', include(router.urls)),
-# ]
+urlpatterns = [
+    path('', include(router.urls)),
+    path('agent/<str:username>/', AgentDetailByUsernameView.as_view(), name='agent-detail-by-username'),
+    path("properties/filter/", FilterPropertiesView.as_view(), name="property-filter"),
+    path("properties/", PropertyListView.as_view(), name="property-list"),
+    path("property/<int:id>/", PropertyDetailView.as_view(), name="property-detail"),
+    path("cities/", CityListView.as_view(), name="city-list"),
+    path('register/', AgentRegisterView.as_view(), name='register-agent'),
+    path('agent/update/<int:id>/', AgentUpdateView.as_view(), name='agent-update'),
+    path('agent/delete/<int:id>/', AgentDeleteView.as_view(), name='agent-delete'),
+    path('agents/list/', AgentListView.as_view(), name='agent-list'),
+    path('properties/status-counts/', PropertyStatusCountView.as_view(), name='property-status-counts'),
+    path('properties/city/count/', PropertyByStatusView.as_view(), name='property-city-wise-count'),
+    path('consultation', ConsultationView.as_view(), name='consultation_details'),
+    path('subscribe/', SubscribeView.as_view(), name='subscribe'),
+    path('developers/', DeveloperListView.as_view(), name='developer-list'),
+]
