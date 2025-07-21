55db659825aa9764da661121b1315093d74b9711
sreehari-a-h <sreehari.delemon@gmail.com>
2025-07-21 13:34:09 +0530

    refactor: updated the settings file.

--- offplan_backend_agent/backend/settings.py
+++ offplan_backend_agent/backend/settings.py
@@ -108,20 +108,20 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv("DB_NAME"),
-#         'USER': os.getenv("DB_USER"),
-#         'PASSWORD': os.getenv("DB_PASSWORD"),
-#         'HOST': os.getenv("DB_HOST"),
-#         'PORT': os.getenv("DB_PORT"),
-#         'OPTIONS': {
-#             'options': '-c search_path=public',
-#             'sslmode': 'require'
-#         }
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+        'OPTIONS': {
+            'options': '-c search_path=public',
+            'sslmode': 'require'
+        }
+    }
+}
 
 
 # DATABASES = {
@@ -131,17 +131,17 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres.xtzdmalumstpitlmiscl',
-        'PASSWORD': 'D3l3m0n@2025',
-        'HOST': 'aws-0-ap-south-1.pooler.supabase.com',
-        'PORT': '5432',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres.xtzdmalumstpitlmiscl',
+#         'PASSWORD': 'D3l3m0n@2025',
+#         'HOST': 'aws-0-ap-south-1.pooler.supabase.com',
+#         'PORT': '5432',
         
-    }
-}
+#     }
+# }
 
 SWAGGER_SETTINGS = {
     'USE_SESSION_AUTH': False,  # 👈 Prevents Django login for Swagger
