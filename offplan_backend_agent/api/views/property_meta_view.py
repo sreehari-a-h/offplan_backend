b426d3074eddc4c7a75a15a98699b3b6adb09255
sreehari-a-h <sreehari.delemon@gmail.com>
2025-12-03 16:44:39 +0530

    added: crawler friendly url for individual property to backend. v1.2

--- offplan_backend_agent/api/views/property_meta_view.py
+++ offplan_backend_agent/api/views/property_meta_view.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from api.models import Property
 
-def property_meta_view(request, username, property_id):
+def property_meta_view(request, username):
+    property_id = request.GET.get("id")
+
+    if not property_id:
+        return HttpResponse("Property ID missing", status=400)
+
     try:
         property_obj = Property.objects.get(id=property_id)
     except Property.DoesNotExist:
@@ -12,3 +17,4 @@ def property_meta_view(request, username, property_id):
         "property": property_obj,
         "username": username,
     })
+
