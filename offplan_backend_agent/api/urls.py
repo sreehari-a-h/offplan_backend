0860819197c27875def28e552a2a7ea9492acff8
sreehari-a-h <sreehari.delemon@gmail.com>
2025-12-19 14:22:09 +0530

    fix: meta info for crawlers:blog v1.5

--- offplan_backend_agent/api/urls.py
+++ offplan_backend_agent/api/urls.py
@@ -44,10 +44,9 @@ urlpatterns = [
     path('reserve-now/<int:id>/',ReserveNowView.as_view(),name='reserve-now'),
     path('api/blogs/', BlogPostList.as_view()),
     path('api/blogs/<slug:slug>/', BlogPostDetail.as_view()),
-    # Blog SEO endpoints (for search engine crawlers)
-    path('/blogs', BlogPostList.as_view()),
-    path('/blog/<slug:slug>', BlogPostDetail.as_view()),
-    path('/blog/<slug:slug>/', BlogPostDetail.as_view()),
+    # Blog SEO pages (HTML)
+    re_path(r'^blogs/?$', BlogListView.as_view(), name='blog-list'),
+    re_path(r'^blog/(?P<slug>[-\w]+)/?$', BlogDetailView.as_view(), name='blog-detail'),
     re_path(r'^agent/(?P<username>[a-zA-Z0-9_-]+)/$', AgentDetailByUsernameView.as_view(), name='agent-detail-by-username'),
 
 ]
