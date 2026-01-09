407e140b574b87175afa029c3aecdd7ade26a8ac
sreehari-a-h <sreehari.delemon@gmail.com>
2026-01-09 19:32:34 +0530

    refactor: Contact page submit form.

--- offplan_backend_agent/api/views/contact_enquiry.py
+++ offplan_backend_agent/api/views/contact_enquiry.py
@@ -3,7 +3,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from api.models import Contact
 from api.serializers import ContactSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
+@api_view(['POST'])
+@permission_classes([AllowAny])
 class ContactEnquiryView(APIView):
     def post(self, request):
         serializer = ContactSerializer(data=request.data)
