1d8dbbc57c105770aa0a293f529196fb27d260c6
sreehari-a-h <sreehari.delemon@gmail.com>
2026-01-09 19:39:07 +0530

    refactor: Contact page submit form. v1.1

--- offplan_backend_agent/api/views/contact_enquiry.py
+++ offplan_backend_agent/api/views/contact_enquiry.py
@@ -1,22 +1,25 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
+
 from api.models import Contact
 from api.serializers import ContactSerializer
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
-@api_view(['POST'])
-@permission_classes([AllowAny])
+
 class ContactEnquiryView(APIView):
+    permission_classes = [AllowAny]
+
     def post(self, request):
         serializer = ContactSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({
-                'message': "Contact enquiry submitted successfully",
-                'data':serializer.data,
-                }, status=status.HTTP_201_CREATED)
+            return Response(
+                {
+                    "message": "Contact enquiry submitted successfully",
+                    "data": serializer.data,
+                },
+                status=status.HTTP_201_CREATED
+            )
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
